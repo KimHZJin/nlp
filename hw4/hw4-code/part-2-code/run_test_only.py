13c6e3e0e28d75c9da9c6e9e42e1ff621e3ea780
Kim Jin <144234491+KimHZJin@users.noreply.github.com>
2025-11-13 21:25:07 -0500

    Add run_test_only.py for testing T5 model

---
+++ hw4/hw4-code/part-2-code/run_test_only.py
@@ -0,0 +1,20 @@
+from train_t5 import *
+
+args = get_args()
+args.finetune = True
+args.experiment_name = 'verified'  # Changed from 'improved'
+args.batch_size = 16
+args.test_batch_size = 16
+
+# Load test data
+_, _, test_loader = load_t5_data(args.batch_size, args.test_batch_size)
+
+# Load best model
+model = load_model_from_checkpoint(args, best=True)
+model.eval()
+
+# Run test inference
+model_sql_path = 'results/t5_ft_experiment_test.sql'
+model_record_path = 'records/t5_ft_experiment_test.pkl'
+test_inference(args, model, test_loader, model_sql_path, model_record_path)
+print("Test predictions saved!")
